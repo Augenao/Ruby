f66f72be910fe5a0134f2c4ce4c63fefee297377
Daniel Nacenta <dnacenta2@gmail.com>
2016-10-13 16:51:36 +0200

    WordSorting

--- WordSorting.rb
+++ WordSorting.rb
@@ -1,14 +1,15 @@
 puts "Write a sentence"
 first_sentence = gets.chomp
-puts "Another one"
-second_sentence = gets.chomp
+#in case you want more sentences
+#puts "Another one"
+#second_sentence = gets.chomp
 
 
 def word_sorting(sentence)
-	puts "#{sentence}".split.sort.join(' ').gsub(/[^0-9a-z ]/i, '')
+	puts "#{sentence}".split.sort_by { |x| x.downcase }.join(' ').gsub(/[^0-9a-z ]/i, '')
 end
 
 word_sorting(first_sentence)
-word_sorting(second_sentence)
+#word_sorting(second_sentence)
 
 
